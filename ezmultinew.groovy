8c81e5c96a17e1dc4c59b3ca33097dbd71fc34ee
lgkahn <kahn@lgk.com>
2021-12-10 22:55:56 -0500

    Update ezmultinew.groovy

--- ezmultinew.groovy
+++ ezmultinew.groovy
@@ -21,7 +21,7 @@
 // v0.0.9 - jrs - got the temp and luminance to work. Motion works. Debugging the color wheel.
 // v0.0.8 - DrZWave 2/25/2015 - change the color control to be tiles since there are only 8 colors.
 // v0.0.7 - jrs - 02/23/2015 - Jim Sulin
-// 1.0.1 lgkhan
+// lgk v1.0 add debug and info logging options and auto turn off for debugging
 
 metadata {
         definition (name: "EZmultiPli new", namespace: "erocm123", author: "Eric Maycock", oauth: true) {
@@ -41,67 +41,7 @@ metadata {
 
         } // end definition
 
-    tiles (scale: 2){
-                multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
-                        tileAttribute ("device.switch", key: "PRIMARY_CONTROL", icon: "st.Lighting.light18") {
-                                attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00a0dc", nextState:"turningOff"
-                                attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff", nextState:"turningOn"
-                                attributeState "turningOn", label:'${name}', icon:"st.switches.light.on", backgroundColor:"#00a0dc"
-                                attributeState "turningOff", label:'${name}', icon:"st.switches.light.off", backgroundColor:"#ffffff"
-                        }
-                        tileAttribute ("device.color", key: "COLOR_CONTROL") {
-                                attributeState "color", action:"setColor"
-                        }
-                        tileAttribute ("statusText", key: "SECONDARY_CONTROL") {
-                                attributeState "statusText", label:'${currentValue}'
-                        }
-        }
-
-                standardTile("motion", "device.motion", width: 2, height: 2, canChangeIcon: true, canChangeBackground: true) {
-                        state "active", label:'motion', icon:"st.motion.motion.active", backgroundColor:"#00a0dc"
-                        state "inactive", label:'no motion', icon:"st.motion.motion.inactive", backgroundColor:"#ffffff"
-                }
-                valueTile("temperature", "device.temperature", width: 2, height: 2) {
-                        state "temperature", label:'${currentValue}°', unit:"F", icon:"", // would be better if the units would switch to the desired units of the system (imperial or metric)
-                        backgroundColors:[
-                        [value: 31, color: "#153591"],
-                    [value: 44, color: "#1e9cbb"],
-                    [value: 59, color: "#90d2a7"],
-                                        [value: 74, color: "#44b621"],
-                                        [value: 84, color: "#f1d801"],
-                                        [value: 95, color: "#d04e00"],
-                                        [value: 96, color: "#bc2323"]
-                        ]
-                }
-
-        // icons to use would be st.Weather.weather2 or st.alarm.temperature.normal - see http://scripts.3dgo.net/smartthings/icons/ for a list of icons
-                valueTile("illuminance", "device.illuminance", width: 2, height: 2, inactiveLabel: false) {
-// jrs 4/7/2015 - Null on display
-                        //state "luminosity", label:'${currentValue} ${unit}'
-                        state "luminosity", label:'${currentValue}', unit:'${currentValue}', icon:"",
-                        backgroundColors:[
-                                [value: 25, color: "#404040"],
-                                [value: 50, color: "#808080"],
-                                [value: 75, color: "#a0a0a0"],
-                                [value: 90, color: "#e0e0e0"],
-                //lux measurement values
-                [value: 150, color: "#404040"],
-                                [value: 300, color: "#808080"],
-                                [value: 600, color: "#a0a0a0"],
-                                [value: 900, color: "#e0e0e0"]
-                        ]
-                }
-                standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-                        state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
-                }
-                standardTile("configure", "device.configure", inactiveLabel: false, width: 2, height: 2, decoration: "flat") {
-                        state "configure", label:'', action:"configuration.configure", icon:"st.secondary.configure"
-                }
-
-                main (["temperature","motion", "switch"])
-                details(["switch", "motion", "temperature", "illuminance", "refresh", "configure"])
-        }  // end tiles
-
+ 
        preferences {
        input "OnTime",  "number", title: "No Motion Interval", description: "N minutes lights stay on after no motion detected [0, 1-127]", range: "0..127", defaultValue: 10, displayDuringSetup: true, required: false
        input "OnLevel", "number", title: "Dimmer Onlevel", description: "Dimmer OnLevel for associated node 2 lights [-1, 0, 1-99]", range: "-1..99", defaultValue: -1, displayDuringSetup: true, required: false
@@ -111,7 +51,9 @@ metadata {
        input("lum", "enum", title:"Illuminance Measurement", description: "Percent or Lux", defaultValue: 1 ,required: false, displayDuringSetup: true, options:
           [1:"Percent",
            2:"Lux"])
-       input name: "debugEnable", type: "bool", title: "Enable Debug Logging", defaultValue: true
+       input name: "debugEnable", type: "bool", title: "Enable Debug Logging", defaultValue: false
+       input("descLog", "bool", title: "Enable descriptionText logging", required: true, defaultValue: true)
+    
   }
 
 } // end metadata
@@ -146,16 +88,16 @@ def zwaveEvent(hubitat.zwave.commands.sensormultilevelv5.SensorMultilevelReport 
                         map.value = convertTemperatureIfNeeded(cmd.scaledSensorValue, cmdScale, cmd.precision)
                         map.unit = getTemperatureScale()
                         map.name = "temperature"
-                        if (debugEnable) log.debug "Temperature report"
+                        if ((debugEnable) || (descLog)) log.info "Temperature report: temp is $map.value"
                         break;
                 case 0x03 :                             // SENSOR_TYPE_LUMINANCE_VERSION_1
                         map.value = cmd.scaledSensorValue.toInteger().toString()
             if(lum == "" || lum == null || lum == 1) map.unit = "%"
             else map.unit = "lux"
                         map.name = "illuminance"
-                        if (debugEnable) log.debug "Luminance report"
+                       if ((debugEnable) || (descLog)) log.info "Luminance report: lux is $map.value"
                         break;
-        }
+        } 
         return map
 }
 
@@ -170,12 +112,12 @@ def zwaveEvent(hubitat.zwave.commands.notificationv3.NotificationReport cmd) {
                         map.name = "motion"
                 map.value = "active"
                         map.descriptionText = "$device.displayName motion detected"
-                if (debugEnable) log.debug "motion recognized"
+                if ((debugEnable) || (descLog)) log.info "motion recognized"
                 } else if (cmd.event==0) {
                         map.name = "motion"
                 map.value = "inactive"
                         map.descriptionText = "$device.displayName no motion detected"
-                if (debugEnable) log.debug "No motion recognized"
+                if ((debugEnable) || (descLog)) log.info "No motion recognized"
                 }
         }
         if (map.name != "motion") {
@@ -192,13 +134,22 @@ def updated()
 {
     if (debugEnable) log.debug "updated() is being called"
     sendEvent(name: "checkInterval", value: 2 * 60 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID])
+    
+    if (debugEnable)
+    {
+       log.info "Debugging is on , will automatically turn off in 30 minutes."
+       runIn(1800, logDebugOff);   
+    }
+    else log.info "Debugging is off."
+    
+    
     def cmds = configure()
 
     if (cmds != []) commands(cmds)
 }
 
 def on() {
-        if (debugEnable) log.debug "Turning Light 'on'"
+        if ((debugEnable) || (descLog)) log.info "Turning Light 'on'"
         commands([
                 zwave.basicV1.basicSet(value: 0xFF),
                 zwave.basicV1.basicGet()
@@ -206,7 +157,7 @@ def on() {
 }
 
 def off() {
-        if (debugEnable) log.debug "Turning Light 'off'"
+        if ((debugEnable) || (descLog)) log.info "Turning Light 'off'"
         commands([
                 zwave.basicV1.basicSet(value: 0x00),
                 zwave.basicV1.basicGet()
@@ -215,7 +166,7 @@ def off() {
 
 
 def setColor(value) {
-    if (debugEnable) log.debug "setColor() : ${value}"
+    if ((debugEnable) || (descLog)) log.info "setColor() : ${value}"
     def myred
     def mygreen
     def myblue
@@ -383,8 +334,6 @@ def configure() {
 }
 
 
-
-
 private command(hubitat.zwave.Command cmd) {
   //  log.debug "in command command = $cmd"
     if (getDataValue("zwaveSecurePairingComplete") != "true") {
@@ -419,7 +368,7 @@ private command(String cmd) {
         def cmds = cmd.split(" ")
         if (cmds[0] == "delay")
         {
-          log.debug "skipping delay"
+         // log.debug "skipping delay"
           return
         }
     }
@@ -494,4 +443,15 @@ private hex(value, width=2) {
                 s = "0" + s
         }
         s
+}
+
+
+void logDebugOff() {
+  //
+  // runIn() callback to disable "Debug" logging after 30 minutes
+  // Cannot be private
+  //
+  log.info "Turning off debugging."
+    
+  device.updateSetting("debugEnable", [type: "bool", value: false]);
 }
