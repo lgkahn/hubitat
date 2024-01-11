65ba735950be715f1762ce286819a5d6abcee481
lgkahn <kahn@lgk.com>
2024-01-11 17:35:07 -0500

    Update wirelesstagapp.groovy

--- wirelesstagapp.groovy
+++ wirelesstagapp.groovy
@@ -29,6 +29,8 @@ preferences {
 
 mappings {
     path('/swapToken') { action: [GET: 'swapToken'] }
+    
+    path('/urlcallback') { action: [ GET: 'handleUrlCallback', POST: 'handleUrlCallback' ] }    
 }
 
 def preferencesPage() {
@@ -72,6 +74,54 @@ def wirelessDeviceList() {
     }
 
     return p
+}
+
+
+def handleUrlCallback () {
+    
+   
+	log.debug " in url callback $params"
+    
+    def id = params.id.toInteger()
+    def type = params.type
+                
+    def dni = getTagUUID(id)
+    
+   if (debug)
+    {
+    log.debug "id = $id"
+    log.debug "type= $type"
+    }
+
+    
+    if (dni) {
+        def d = getChildDevice(dni)
+
+        if(d)
+        {
+        	def data = null
+            
+        	switch (type) {
+            	case "oor": data = [presence: "not present"]; break
+                case "back_in_range": data = [presence: "present"]; break
+                case "motion_detected": data = [acceleration: "active", motion: "active"]; startMotionTimer(d); break
+                
+                // motion timeout callback is not working currently in WST  lgk working again
+                 case "motion_timedout": data = [acceleration: "inactive", motion: "inactive"]; break
+                
+                case "door_opened": data = [contact: "open"]; break
+                case "door_closed": data = [contact: "closed"]; break
+                case "water_detected": data = [water : "wet"]; break                
+                case "water_dried": data = [water : "dry"]; break
+            }
+            
+            log.trace "callback action = " + data?.toString()
+            
+            if (data) {
+            	d.generateEvent(data)
+            }
+        }
+    }
 }
 
 Map getWirelessTags() {
@@ -120,53 +170,66 @@ String getChildName(Map tagInfo) {
     return deviceType
 }
 
-void initialize() {
+
+
+
+def initialize() {
+    
     unschedule()
+    
+	def curDevices = devices.collect { dni ->
 
-    def curDevices = devices.collect { dni ->
-        def d = getChildDevice(dni)
-
+		def d = getChildDevice(dni)
+        
         def tag = atomicState.tags.find { it.uuid == dni }
 
-        if (d) {
-            log.debug "found ${d.displayName} $dni already exists"
-            d.updated()
-        }
-        else
-        {
-            d = addChildDevice(getChildNamespace(), getChildName(tag), dni, null, [label:"${tag?.name} Wireless Tag"])
+		if(!d)
+		{       	
+			d = addChildDevice(getChildNamespace(), getChildName(tag), dni, null, [label:tag?.name])
             d.initialSetup()
-            log.debug "created ${d.displayName} $dni"
+			log.debug "created ${d.displayName} $dni"
+		}
+		else
+		{
+			log.debug "found ${d.displayName} $dni already exists"
+            d.updated()
+		}
+        
+        if (d) { 
+        	// configure device
+        	setupCallbacks(d, tag)
         }
 
-        return dni
-    }
+		return dni
+	}
 
-    def delete
-    // Delete any that are no longer in settings
-    if (curDevices) {
-        delete = getChildDevices().findAll { !curDevices.contains(it.deviceNetworkId) }
-    }
-    else {
-        delete = getAllChildDevices()
-    }
+	def delete
+	// Delete any that are no longer in settings
+	if(!curDevices)
+	{
+		delete = getAllChildDevices()
+	}
+	else
+	{
+		delete = getChildDevices().findAll { !curDevices.contains(it.deviceNetworkId) }
+	}
 
-    delete.each { deleteChildDevice(it.deviceNetworkId) }
+	delete.each { deleteChildDevice(it.deviceNetworkId) }
 
-    if (atomicState.tags == null) { atomicState.tags = [:] }
+	if (atomicState.tags == null) { atomicState.tags = [:] }
 
-    pollHandler()
-
+	pollHandler()
+    
     // set up internal poll timer
-    if (pollTimer == null) {
-        pollTimer = 5
-    }
-    log.trace "setting poll to ${pollTimer}"
+	if (pollTimer == null) pollTimer = 5
+
+	log.trace "setting poll to ${pollTimer}"
     schedule("0 0/${pollTimer.toInteger()} * * * ?", pollHandler)
 }
 
+
 String oauthInitUrl() {
-    log.debug 'oauthInitUrl()'
+    if (debug) log.debug 'oauthInitUrl()'
     atomicState.oauthInitState = UUID.randomUUID().toString()
 
     Map oauthParams = [
@@ -179,7 +242,7 @@ String oauthInitUrl() {
 }
 
 String generateRedirectUrl() {
-    log.debug 'generateRedirectUrl'
+    if (debug) log.debug 'generateRedirectUrl'
     // return apiServerUrl("/api/token/${atomicState.accessToken}/smartapps/installations/${app.id}/swapToken")
     // return apiServerUrl("token/${atomicState.accessToken}/smartapps/installations/${app.id}/swapToken")
     // log.debug 'state.accessToken: ' + state.accessToken
@@ -188,7 +251,7 @@ String generateRedirectUrl() {
 }
 
 void swapToken() {
-    log.debug "swapping token: $params"
+    if (debug) log.debug "swapping token: $params"
 
     // mytaglist.com doesn't properly add parameters when one exists. So, also look for this mangled version.
     String code = params.code ?: params['?code']
@@ -294,9 +357,9 @@ void updateAllDevices() {
 void pollSingle(def child) {
     if (debug) log.trace 'pollSingle()'
     getTagStatusFromServer()
-
+    
     def device = atomicState.tags.find { it.uuid == child.device.deviceNetworkId }
-
+    
     if (device) {
         updateDeviceStatus(device, child)
     }
@@ -313,8 +376,7 @@ void updateDeviceStatus(def device, def d) {
     def int batLevel = (device.batteryRemaining * 100) as int   
     if (batLevel > 100) batLevel = 100
    
-    if (debug)
-    log.debug " **** got eventState = ${device.eventState}"
+    if (debug) log.debug " **** got eventState = ${device.eventState}"
   
     // lgk from my perussing eventstate 1 = armed
     // 0= unarmed
@@ -330,6 +392,19 @@ void updateDeviceStatus(def device, def d) {
             motion = 'active'
         }
     }
+     if (debug) log.debug "----------------tag type = ${device.tagType}"
+    
+    if (device.tagType == 72)
+        {
+            // assume these are armed.
+           if (debug) log.debug "**************** set to armed"
+            motionArmed = true
+        }
+    
+    
+    if (device.eventState == "Moved")
+     motion = 'active'
+    else motion = 'inactive'
     
     if (motionArmed == true)
     {
@@ -341,7 +416,7 @@ void updateDeviceStatus(def device, def d) {
         illuminance: (device.lux)?.toDouble().round(),
         signaldBm: (device.signaldBm) as int,
         motionArmed : motionArmed,
-        motion : motion
+        motion : motion  
       
         // water : (device.shorted == true) ? 'wet' : 'dry',
     ]
@@ -428,7 +503,7 @@ def postMessage(String path, Object query) {
                 }
             } else {
                 if (resp.status == 500 && resp.data.status.code == 14) {
-                    log.debug 'Need to refresh auth token?'
+                    if (debug) log.debug 'Need to refresh auth token?'
                     atomicState.authToken = null
                 }
                 else {
@@ -449,6 +524,8 @@ String getQuoted(String orig) {
 }
 
 def getTagID(String uuid) {
+    if (debug) log.debug "looking for tag with uuid= $uuid"
+    
     return atomicState.tags.find { it.uuid == uuid }?.slaveId
 }
 
@@ -537,3 +614,387 @@ void debugEvent(String message, boolean displayEvent) {
     log.debug "Generating AppDebug Event: ${results}"
     sendEvent (results)
 }
+
+
+def setSingleCallback(def tag, Map callback, def type) {
+
+   if (debug)
+    {
+    log.debug "in set single callback"
+    log.debug ""  
+    log.debug "tag = $tag"
+    }
+    
+	def parameters = "?type=$type&"
+     
+    switch (type) {
+    	case "water_dried": 
+        case "water_detected":
+        	// 2 params
+        parameters = parameters + "name={0}&id={1}" // = 1
+        	break;
+        case "oor": 
+        case "back_in_range":
+        case "motion_timedout":
+        	// 3 params
+        parameters = parameters + "name={0}&time={1}&id={2}"
+        	break;
+    	case "door_opened": 
+        case "door_closed":     
+        parameters = parameters + "name={0}&orientchg={1}&x={2}&y={3}&z={4}&id={5}"
+        	break;
+        case "motion_detected":
+        	// to do, check if PIR type
+           	if (getTagTypeInfo(tag).isPIR == true) {
+            	// pir
+                parameters = parameters + "name={0}&time={1}&id={2}"
+            } else {
+            	// standard
+                parameters = parameters + "name={0}&orientchg={1}&x={2}&y={3}&z={4}&id={5}"
+            }
+        	break;    }
+    
+    def theurl =  fullApiServerUrl("urlcallback${parameters}&access_token=${atomicState?.accessToken}&")
+    if (debug) log.debug "here  url = $theurl"
+  //  String callbackString = """{"url":"${fullLocalApiServerUrl("acce
+
+//${getApiServerUrl()}/api/token/${atomicState.accessToken}/smartapps/installations/${app.id}/urlcallback${parameters}","verb":"GET","content":"","disabled":false,"nat":false}"""
+  
+   String callbackString = """{"url":"${theurl}","verb":"GET","content":"","disabled":false,"nat":false}"""
+     
+    if (debug) log.debug "here callback string = ${callbackString}"
+    
+   //String callbackString = """{"url":"${getApiServerUrl()}/api/token/${atomicState.accessToken}/smartapps/installations/${app.id}/urlcallback${parameters}","verb":"GET","content":"","disabled":false,"nat":false}"""
+    return callbackString
+}
+
+def getQuoted(def orig) { return (orig != null) ? "\"${orig}\"": orig }
+
+def useExitingCallback(Map callback) {
+    
+   if (debug)
+    {
+    log.debug "*** in use existing callback - *${callback}*"
+    log.debug ""
+    log.debug "call back url passed in = ${callback.url}"
+    }
+    // ignore blank
+    if (callback.url != "http://")
+    {
+   
+    def theurl = fullApiServerUrl("${callbackup.url}&access_token=${atomicState?.accessToken}&")
+    if (debug) log.debug "here 2 url = ${theurl}"
+    String callbackString = """{"url":"${theurl}","verb":${getQuoted(callback.verb)},"content":${getQuoted(callback.content)},"disabled":${callback.disabled},"nat":${callback.nat}}"""
+   
+    if (debug) log.debug "here 2 callback string = ${callbackString}"
+    
+	//String callbackString = """{"url":"${callback.url}","verb":${getQuoted(callback.verb)},"content":${getQuoted(callback.content)},"disabled":${callback.disabled},"nat":${callback.nat}}"""
+    return callbackString
+    }
+    else return callback.url
+}
+
+
+def setupCallbacks(def child, def tag) {
+	def id = getTagID(child.device.deviceNetworkId)
+    
+    if (id != null) {
+        Map query = [
+            "id": id
+        ]
+        def respMap = postMessage("/ethClient.asmx/LoadEventURLConfig", query)
+        
+        if (respMap.d != null) {
+            
+            // note setup callbacks only need to be done for certain tags. 
+            
+            // lgk no use existing causes errors
+            /*
+             String message = """{"id":${id},
+            	"config":{
+                "__type":"MyTagList.EventURLConfig",
+                "oor":${setSingleCallback(tag, respMap.d?.oor, "oor")},
+                "back_in_range":${setSingleCallback(tag, respMap.d?.back_in_range, "back_in_range")},
+                "low_battery":${useExitingCallback(respMap.d?.low_battery)},
+                "motion_detected":${setSingleCallback(tag, respMap.d?.motion_detected, "motion_detected")},
+                "door_opened":${setSingleCallback(tag, respMap.d?.door_opened, "door_opened")},
+                "door_closed":${setSingleCallback(tag, respMap.d?.door_closed, "door_closed")},
+                "door_open_toolong":${useExitingCallback(respMap.d?.door_open_toolong)},
+                "temp_toohigh":${useExitingCallback(respMap.d?.temp_toohigh)},
+                "temp_toolow":${useExitingCallback(respMap.d?.temp_toolow)},
+                "temp_normal":${useExitingCallback(respMap.d?.temp_normal)},
+                "cap_normal":${useExitingCallback(respMap.d?.cap_normal)},
+                "too_dry":${useExitingCallback(respMap.d?.too_dry)},
+                "too_humid":${useExitingCallback(respMap.d?.too_humid)},
+                "water_detected":${setSingleCallback(tag, respMap.d?.water_detected, "water_detected")},
+                "water_dried":${setSingleCallback(tag, respMap.d?.water_dried, "water_dried")},
+                "motion_timedout":${setSingleCallback(tag, respMap.d?.motion_timedout, "motion_timedout")}
+                },
+                "applyAll":false}"""
+            */
+            String message = """{"id":${id},
+            	"config":{
+                "__type":"MyTagList.EventURLConfig",
+                "oor":${setSingleCallback(tag, respMap.d?.oor, "oor")},
+                "back_in_range":${setSingleCallback(tag, respMap.d?.back_in_range, "back_in_range")},
+              
+                "motion_detected":${setSingleCallback(tag, respMap.d?.motion_detected, "motion_detected")},
+                "door_opened":${setSingleCallback(tag, respMap.d?.door_opened, "door_opened")},
+                "door_closed":${setSingleCallback(tag, respMap.d?.door_closed, "door_closed")},
+               
+             
+                "water_detected":${setSingleCallback(tag, respMap.d?.water_detected, "water_detected")},
+                "water_dried":${setSingleCallback(tag, respMap.d?.water_dried, "water_dried")},
+                "motion_timedout":${setSingleCallback(tag, respMap.d?.motion_timedout, "motion_timedout")}
+                },
+                "applyAll":false}"""
+                
+            if (debug) log.debug "sending message eventurl config $message"
+            
+            postMessage("/ethClient.asmx/SaveEventURLConfig", message)
+            
+        }
+    }
+}
+
+def beep(def child, int len) {
+	def id = getTagID(child.device.deviceNetworkId)
+    
+    if (id != null) {
+        Map query = [
+            "id": id,
+            "beepDuration": len
+        ]
+        postMessage("/ethClient.asmx/Beep", query)
+    } else {
+    	log.trace "Could not find tag"
+    }
+    
+    return null
+}
+
+def light(def child, def on, def flash) {
+	def id = getTagID(child.device.deviceNetworkId)
+    
+    def command = (on == true) ? "/ethClient.asmx/LightOn" : "/ethClient.asmx/LightOff"
+    
+    if (id != null) {
+        Map query = [
+            "id": id,
+            "flash": flash
+        ]
+        postMessage(command, query)
+    } else {
+    	log.trace "Could not find tag"
+    }
+    
+    return null
+}
+
+def startMotionTimer(def child) {   
+	log.trace "start motion timer"
+
+	if (state.motionTimers == null) {
+		state.motionTimers = [:]
+    }
+    
+    def delayTime = child.getMotionDecay()
+    
+    // don't do less than a minute in this way, once WST has the callback working it will be better
+    delayTime = (delayTime < 60) ? 60 : delayTime
+    
+    state.motionTimers[child.device.deviceNetworkId] = now() + delayTime
+    
+	runIn(delayTime, motionTimerHander)	
+}
+
+def motionTimerHander() {
+    def more = 0
+    def removeList = []
+    
+    state.motionTimers.each { child, time ->
+        if (time <= now()) {
+            def tag = getChildDevice(child)
+            resMotionDetection(tag)
+			removeList.add(child)
+        } else {
+            if ((more == 0) || (more > time)) {
+                more = time
+            }
+        }
+    }
+    
+    if (more != 0) { 
+    	log.trace "running again"
+        more = more + 5 - now()
+    	runIn((more < 60) ? 60 : more, motionTimerHander)
+    }    
+    
+    // clean up handled events
+    removeList.each {
+    	state.motionTimers.remove(it)
+    }
+}
+
+def resMotionDetection(def child) {
+	log.trace "turning off motion"
+    
+    // now turn off in device
+    def data = [acceleration: "inactive", motion: "inactive"]
+    child.generateEvent(data)
+    
+    return null
+}
+
+def armMotion(def child) {
+	def id = getTagID(child.device.deviceNetworkId)
+    
+    if (id != null) {
+        Map query = [
+            "id": id,
+            "door_mode_set_closed": true
+        ]
+        postMessage("/ethClient.asmx/Arm", query)
+    } else {
+    	log.trace "Could not find tag"
+    }
+    
+    return null 
+}
+
+def disarmMotion(def child) {
+	def id = getTagID(child.device.deviceNetworkId)
+    
+    if (id != null) {
+        Map query = [
+            "id": id
+        ]
+        postMessage("/ethClient.asmx/DisArm", query)
+    } else {
+    	log.trace "Could not find tag"
+    }
+    
+    return null 
+}
+
+
+def setMotionMode(def child, def mode, def timeDelay) {
+	log.trace "setting door to closed mode = $mode"
+        
+	def id = getTagID(child.device.deviceNetworkId)
+    
+    if (id != null) {
+    	if (mode == "disarmed") {
+        	disarmMotion(child)
+        } else {        
+            Map query = [
+                "id": id
+            ]
+            def result = postMessage("/ethClient.asmx/LoadMotionSensorConfig", query)
+
+            if (result?.d) {
+  
+                switch (mode) {
+                    case "accel":
+                        result.d.door_mode = false
+                        break
+                    case "door":
+                        result.d.door_mode = true
+                        break
+                }
+
+                result.d.auto_reset_delay = timeDelay
+
+                String jsonString = toJson(result.d)
+                jsonString = toJson(result.d).substring(1, toJson(result.d).size()-1)
+
+                String queryString = """{"id":${id},
+                "config":{"__type":"MyTagList.MotionSensorConfig",${jsonString}},
+                "applyAll":false}"""
+
+                postMessage("/ethClient.asmx/SaveMotionSensorConfig", queryString)
+                            
+    			armMotion(child)
+            }
+        }
+    } else {
+    	log.trace "Could not find tag"
+    }
+    
+    return null    
+}
+
+
+def getTagID(def uuid) {
+	
+    if (debug) log.debug " looking for tag with uuid = $uuid"
+    
+    return atomicState.tags.find{ it.uuid == uuid}?.slaveId
+}
+
+def getTagUUID(def id) {
+	
+    return atomicState.tags.find{ it.slaveId == id}?.uuid
+}
+
+def getTagTypeInfo(def tag) {
+	Map tagInfo = [:]
+
+	tagInfo.isMsTag = (tag.tagType == 12 || tag.tagType == 13);
+	tagInfo.isMoistureTag = (tag.tagType == 32 || tag.tagType == 33);
+	tagInfo.hasBeeper = (tag.tagType == 13 || tag.tagType == 12);
+	tagInfo.isReed = (tag.tagType == 52 || tag.tagType == 53);
+	tagInfo.isPIR = (tag.tagType == 72);
+	tagInfo.isKumostat = (tag.tagType == 62);
+	tagInfo.isHTU = (tag.tagType == 52 || tag.tagType == 62 || tag.tagType == 72 || tag.tagType == 13);
+    
+    return tagInfo
+} 
+
+def getTagVersion(def tag) {
+	if (tag.version1 == 2) {
+		if (tag.rev == 14) return " (v2.1)";
+		else return " (v2.0)";
+	}
+	if (tag.tagType != 12) return "";
+	if (tag.rev == 0) return " (v1.1)";
+	else if (tag.rev == 1) return ' (v1.2)';
+	else if (tag.rev == 11) return " (v1.3)";
+	else if (tag.rev == 12) return " (v1.4)";
+	else if (tag.rev == 13) return " (v1.5)";
+	else return "";
+}
+
+def convertTagTypeToString(def tag) {
+	def tagString = "Unknown"
+    
+    switch (tag.tagType) {
+    	case 12:
+        	tagString = "MotionSensor"
+        	break;
+        case 13:
+        	tagString = "MotionHTU"
+        	break;
+        case 72:
+        	tagString = "PIR"
+            break;
+        case 52:
+            tagString = "ReedHTU"
+            break;   
+        case 53:
+        	tagString = "Reed"
+            break;   
+        case 62:
+        	tagString = "Kumostat"
+            break; 
+        case 32:
+        case 33:
+        	tagString = "Moisture"
+            break;     
+	}
+
+	return tagString + getTagVersion(tag)
+}
+
+def getSmartThingsClientId() { "67953bd9-8adf-422a-a7f0-5dbf256b9024" }
+
